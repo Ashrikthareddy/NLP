d67b081cf8f59130d69e3ab18226776765d7e169
Ashrikthareddy <annapureddyashrikthareddy@gmail.com>
2026-08-05 08:58:08 +0530

    Delete Assessment Tools/5.py

--- Assessment Tools/5.py
+++
@@ -1,9 +0,0 @@
-from nltk.stem import PorterStemmer
-
-ps = PorterStemmer()
-
-words = input("Enter words separated by space: ").split()
-
-print("\nWord\tStem")
-for word in words:
-    print(word, "\t", ps.stem(word))
